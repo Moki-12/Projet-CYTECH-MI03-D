58995734369713364d6c3a9364cc3f58c9508e8e
raphael573 <raphael.yoyotte@etu.cyu.fr>
2026-04-25 00:20:33 +0200

    Update diviserManche.c

--- src/diviserManche.c
+++ src/diviserManche.c
@@ -1,3 +1,4 @@
+//115 A 120
 void InitManche (joueur joueur[], int nb_joueur, int penalite[]){
     for (int i=0; i<nb_joueur; i++){
         joueur[i].scores = 0;
@@ -24,7 +25,7 @@ void CartesMain (joueur joueur[], int j){
 }
 
 //177 A 230
-void StatsPioche (joueur joueur[], carte tab[], int j, int n){
+void StatsPioche (carte tab[], int n){
     cartes compteur[13] = {0};
     for (int i = 0; i <n; i++) {
                         compteur[tab[i].numero].numero++;
@@ -78,7 +79,7 @@ void StatsPioche (joueur joueur[], carte tab[], int j, int n){
                 }
 
 //233 A 268
-void Conseil (cartes tab[], joueur joueur[], int j, int *taille){
+void Conseil (cartes tab[], joueur joueur[], int j, int taille, int n){
                     int stats_danger[13] = {0};    //à partir de la ligne 194 c'est entre la fin des statistiques de la pioche et le début de que voulez vous faire 
                 for(int i = 0; i < n; i++){
                     if(tab[i].bonus[0] == '\0' && tab[i].numero >= 0 && tab[i].numero <= 12){
@@ -89,11 +90,11 @@ void Conseil (cartes tab[], joueur joueur[], int j, int *taille){
                 }
                 int dangerTotal = 0;
                 printf("\n-------- ANALYSE DU RISQUE ---------\n");
-                int debutMain = joueurs[j].nb_cartes - joueurs[j].nbCartesManche;
-                for(int c = 0; c < joueurs[j].nbCartesManche; c++){
+                int debutMain = joueur[j].nb_cartes - joueur[j].nbCartesManche;
+                for(int c = 0; c < joueur[j].nbCartesManche; c++){
                     int index = debutMain + c;
-                    if(joueurs[j].cartes[index].bonus[0] != '\0') continue;
-                    int num_carte = joueurs[j].cartes[index].numero;
+                    if(joueur[j].cartes[index].bonus[0] != '\0') continue;
+                    int num_carte = joueur[j].cartes[index].numero;
                     int totalmain;
                     if(num_carte == 0 || num_carte == 1){
                         totalmain = 1;
@@ -112,10 +113,10 @@ void Conseil (cartes tab[], joueur joueur[], int j, int *taille){
 }
 
 float risque = 0;
-if (*taille > 0) {
-    risque = (dangerTotal * 100.0) / (*taille);
+if (taille > 0) {
+    risque = (dangerTotal * 100.0) / (taille);
 }
-printf("Total cartes dangereuses : %d | Cartes restantes : %d\n", dangerTotal, *taille);
+printf("Total cartes dangereuses : %d | Cartes restantes : %d\n", dangerTotal, taille);
 printf("Probabilite de doublon : %.1f%%\n", risque);
 
 
@@ -150,14 +151,14 @@ void AfficherCarte (joueur joueur[], int j){
         printf ("Carte piochée : [%d]\n", joueur[j].cartes[joueur[j].nb_cartes].numero);
     }
     else {
-        printf ("Carte piochée : [%d]\n", joueur[j].cartes[joueur[j].nb_cartes].numero);
+        printf ("Carte piochée : [%s]\n", joueur[j].cartes[joueur[j].nb_cartes].bonus);
     }
 }
 // 312 A 319
 void Doublon (joueur joueur[], int j, int penalite[]){
     int debut = joueur[j].nb_cartes - joueur[j].nbCarteManche;
-    if (carteExiste (joueur[j].cartes, debut, joueur[j].nb_cartes, joueur[j].cartes[joueur[j].nb_cartes])){
-        joueur[j].score = 0;
+    if (carteExisteManche (joueur[j].cartes, debut, joueur[j].nbCarteManche, joueur[j].cartes[joueur[j].nb_cartes])){
+        joueur[j].scores = 0;
         joueur[j].score_pot = 0;
         penalite[j] = 1;
         printf ("\nDommage vous avez pioché un doublon (%d) !\nVotre score pour cette manche est 0 !\n\n", joueur[j].cartes[joueur[j].nb_cartes].numero);
@@ -175,16 +176,13 @@ void BonnePioche (joueur joueur[], int j){
     }
 }
  //338 A 343
-void Flip7 (joueur joueur[], int j, int *i, int *n){
-    int I = *i;
-    int N = *n;
+int Flip7 (joueur joueur[], int j){
     if (joueur[j].nbCarteManche >= 7){
         printf ("FELICITATION %s ! Vous venez de réaliser un Flip7 !\nVotre score pour cette manche est %d + 15\n", joueur[j].pseudo, joueur[j].scores);
         joueur[j].score_pot += 15;
-        I=7;
-        N++;
-        break;
+        return 1; //signal
     }
+    return 0;
 }
 
 
@@ -196,7 +194,7 @@ void manche(cartes tab[], int nb_joueur, joueur joueurs[], int *taille, int *der
     int penalise[50];
     int cartesPiochées = 0;
     InitManche(joueurs, nb_joueur, penalise);
-    for (int i=0; i<n; i++){
+    for (int i=0; i<7; i++){
         printf ("Tour n°%d\n", i+1);
         nb_tour++;
         for (int j=0; j<nb_joueur; j++){
@@ -230,9 +228,9 @@ void manche(cartes tab[], int nb_joueur, joueur joueurs[], int *taille, int *der
                     CartesMain(joueurs, j);
                     printf ("Votre score potentiel : %d\n----------------------------\n",
                             joueurs[j].score_pot);
-                            StatsPioche(tab);
+                            StatsPioche(tab, n);
                 }
-                Conseil(tab, joueurs, j, *taille);
+                Conseil(tab, joueurs, j, &*taille, n);
                 sortir = Piocher(joueurs, j, nb_tour, sortir);
                 }
                 while (sortir != 1 && sortir != 2);
@@ -249,7 +247,11 @@ void manche(cartes tab[], int nb_joueur, joueur joueurs[], int *taille, int *der
                     joueurs[j].nb_cartes++;
                     joueurs[j].nbCartesManche++;
                     joueurs[j].score_pot = joueurs[j].scores;
-                    Flip7(joueurs, j, &i, &n);
+                    if (Flip7(joueurs, j)){
+                        i=7;
+                        n++;
+                        break;
+                    }
                     }
                     n++;
                 }
