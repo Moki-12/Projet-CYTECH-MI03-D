39916bd7177d2015438751c652159c95337e2a54
Moki-12 <161987627+Moki-12@users.noreply.github.com>
2026-05-15 01:08:05 +0200

    Create joueur.h

---
+++ include/joueur.h
@@ -0,0 +1,18 @@
+#ifndef JOUEUR_H
+#define JOUEUR_H
+
+#include "carte.h" // car la structure joueur utilise le type 'cartes'
+
+typedef struct {
+    char   pseudo[100];
+    int    scores;
+    cartes cartes[500]; // Sécurité contre l'overflow
+    int    nb_cartes;
+    int    score_total;
+    int    nbCartesManche;
+    int    score_pot;
+    int    debutManche;
+    int    flip7;
+} joueur;
+
+#endif
