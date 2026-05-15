751633faea2ab6a073e9e237f5870d3c7635e44a
Moki-12 <161987627+Moki-12@users.noreply.github.com>
2026-05-16 00:56:14 +0200

    Update carte.h

--- include/carte.h
+++ include/carte.h
@@ -9,7 +9,7 @@ typedef struct {
        
 void corrigeTab(cartes *tab);
 int melanger(cartes *tab, int taille);
-int effetNumero(cartes *tab, int taille);
+int effetNumero(int numero, int score);
 int effetBonus(char *bonus, int score);
 int carteExisteManche(cartes *main, int debut, int taille, cartes carte);
 
