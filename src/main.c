fa3389a2cc6e955699027979baeea4a5cbbb44fb
Moki-12 <161987627+Moki-12@users.noreply.github.com>
2026-05-16 00:53:03 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,8 +1,9 @@
-#include <stdio.h>     
-#include <stdlib.h>     
-#include <time.h>      
-#include "joueur.h"    
-#include "carte.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>   
+#include "carte.h"    
+#include "joueur.h"   
 #include "affichage.h"
 #include "jeu.h"
 
