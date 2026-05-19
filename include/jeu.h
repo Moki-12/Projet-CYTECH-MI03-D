a45adeec049e67f39109d81d1506eedac9a64231
raphael573 <raphael.yoyotte@etu.cyu.fr>
2026-05-19 15:27:50 +0200

    Update function declarations in jeu.h

--- include/jeu.h
+++ include/jeu.h
@@ -1,11 +1,12 @@
 #ifndef JEU_H
 #define JEU_H
-
+ 
 #include "joueur.h"
 #include "carte.h"
-
+ 
 void manche(cartes *tab, int nb_joueur, joueur *joueurs, int *taille, int *dernierePioche);
-void pause();
-void vide_buffer();
-
+void pause(void);
+void vide_buffer(void);
+ 
 #endif
+ 
