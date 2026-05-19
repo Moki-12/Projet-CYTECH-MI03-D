e6851bd23e90d859243106558b3b01440588a16b
raphael573 <raphael.yoyotte@etu.cyu.fr>
2026-05-19 15:27:09 +0200

    Refactor carte.h header guard and function declarations

--- include/carte.h
+++ include/carte.h
@@ -1,16 +1,16 @@
-#ifndef CARTES_H
-#define CARTES_H
-
-  
+#ifndef CARTE_H
+#define CARTE_H
+ 
 typedef struct {
-    int numero;
+    int  numero;
     char bonus[5];
 } cartes;
-       
-void corrigeTab(cartes *tab);
-int melanger(cartes *tab, int taille);
-int effetNumero(int numero, int score);
-int effetBonus(char *bonus, int score);
-int carteExisteManche(cartes *main, int debut, int taille, cartes carte);
-
+ 
+void creerPioche(cartes *tab);
+int  melanger(cartes *tab, int taille);
+int  effetNumero(int numero, int score);
+int  effetBonus(char *bonus, int score);
+int  carteExisteManche(cartes *main, int debut, int taille, cartes carte);
+ 
 #endif
+ 
