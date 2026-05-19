71ffe48b7524cfe2bf0ba73b74438c1093b9c127
raphael573 <raphael.yoyotte@etu.cyu.fr>
2026-05-19 14:55:37 +0200

    Update carte.c

--- src/carte.c
+++ src/carte.c
@@ -2,16 +2,18 @@
 #include <string.h>    
 #include <stdlib.h>
 
-
-void corrigeTab(cartes *tab) {
+// fonction qui creer toute les cartes de la pioche du jeu
+void creerPioche(cartes *tab) {
     // creation des cartes numeros
     int i = 0;
     tab[i].numero = 0; tab[i].bonus[0] = '\0'; i++;
     tab[i].numero = 1; tab[i].bonus[0] = '\0'; i++;
+    // les deux premiers cartes (0 et 1) sont en un seule exemplaires
     for (int n = 2; n <= 12; n++)
         for (int j = 0; j < n; j++) {
             tab[i].numero = n; tab[i].bonus[0] = '\0'; i++;
         }
+    
     //creation des cartes bonus
     tab[i].numero = -1; strcpy(tab[i++].bonus, "x2");
     tab[i].numero = -2; strcpy(tab[i++].bonus, "+2");
@@ -20,7 +22,7 @@ void corrigeTab(cartes *tab) {
     tab[i].numero = -5; strcpy(tab[i++].bonus, "+8");
     tab[i].numero = -6; strcpy(tab[i++].bonus, "+10");
 }
-
+// fonctions aléatoires
 int melanger(cartes *tab, int taille) {
     if (tab == NULL || taille <= 0) return 0;
     for (int i = taille - 1; i > 0; i--) {
@@ -29,12 +31,12 @@ int melanger(cartes *tab, int taille) {
     }
     return 1;
 }
-
+// score de chaque carte numero
 int effetNumero(int numero, int score) {
     if (numero < 0 || numero > 12 || score < 0) exit(1);
     return score + numero;
 }
-
+//score de chaque carte bonus
 int effetBonus(char *bonus, int score) {
     if (bonus == NULL || score < 0) exit(2);
     if (strcmp(bonus, "x2")  == 0) return score * 2;
@@ -45,14 +47,13 @@ int effetBonus(char *bonus, int score) {
     if (strcmp(bonus, "+10") == 0) return score + 10;
     return score;
 }
-
+// veriication de la presence d'un doublon
 int carteExisteManche(cartes *main, int debut, int taille, cartes carte) {
     if (main == NULL) exit(4);
     for (int i = debut; i < debut + taille; i++)
         if (main[i].numero == carte.numero) return 1;
     return 0;
 }
-
 // Compte les numéros (sans bonus) depuis le début de la manche 
 int compterNumeros(joueur *j) {
     if (j == NULL) exit(3);
