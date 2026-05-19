9325345f4c574919f77e7c644bd7555373293b39
raphael573 <raphael.yoyotte@etu.cyu.fr>
2026-05-19 15:30:20 +0200

    Update sauvegarde.c

--- src/sauvegarde.c
+++ src/sauvegarde.c
@@ -1,11 +1,22 @@
-void CahierDesCharges(int nb_joueur, joueur *joueurs, char *nom) {
-    if (nb_joueur <= 0 || joueurs == NULL || nom == NULL) exit(1);
+#include <stdio.h>
+#include <stdlib.h>
+#include "sauvegarde.h"
+ 
+void CahierDesCharges(int nb_joueur, joueur *joueurs, char *nom_fichier) {
+    if (nb_joueur <= 0 || joueurs == NULL || nom_fichier == NULL) exit(1);
+ 
     char nomComplet[110];
-    sprintf(nomComplet, "%s.txt", nom);
+    sprintf(nomComplet, "%s.txt", nom_fichier);
+ 
     FILE *f = fopen(nomComplet, "w");
-    if (!f) { printf("Erreur : Impossible de créer le fichier %s\n", nomComplet); return; }
+    if (!f) {
+        printf("Erreur : Impossible de créer le fichier %s\n", nomComplet);
+        return;
+    }
+ 
     for (int i = 0; i < nb_joueur; i++) {
-        fprintf(f, " Joueur : %s | Score Final : %d\n", joueurs[i].pseudo, joueurs[i].score_total);
+        fprintf(f, " Joueur : %s | Score Final : %d\n",
+                joueurs[i].pseudo, joueurs[i].score_total);
         fprintf(f, " Cartes obtenues : ");
         for (int j = 0; j < joueurs[i].nb_cartes; j++) {
             if (joueurs[i].cartes[j].bonus[0] == '\0')
@@ -15,6 +26,8 @@ void CahierDesCharges(int nb_joueur, joueur *joueurs, char *nom) {
         }
         fprintf(f, "\n  ──────────────────────────────\n");
     }
+ 
     fclose(f);
     printf(" La partie a ete sauvegardee sous le nom : %s\n", nomComplet);
 }
+ 
