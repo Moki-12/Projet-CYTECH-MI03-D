9d39ac43d85ca3a785ec90e22915783408660c8a
Moki-12 <161987627+Moki-12@users.noreply.github.com>
2026-05-19 23:49:44 +0200

    Update joueur.h

--- include/joueur.h
+++ include/joueur.h
@@ -6,12 +6,12 @@
 typedef struct {
     char   pseudo[100];
     int    scores;         // le score obtenu après avoir pioché une carte
-    cartes cartes[500];
-    int    nb_cartes;
+    cartes cartes[500];    // tableau de toutes les cartes piochées sur toute la partie (500 pour éviter l'overflow)
+    int    nb_cartes;       // nombre total de carte piochée depuis le début de la partie
     int    score_total;   // le score total en fin de manche 
     int    nbCartesManche;
     int    score_pot;    // le score potentiel en fin d'un tour
-    int    debutManche;
+    int    debutManche;   // indice du tab carte de la première carte piochée par le joueur dans la manche 
     int    flip7;
 } joueur;
  
