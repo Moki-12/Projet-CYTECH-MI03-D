e4c703432f5a2cf3292a445c1507164afa0fbb2a
Moki-12 <161987627+Moki-12@users.noreply.github.com>
2026-05-19 23:31:57 +0200

    Update carte.h

--- include/carte.h
+++ include/carte.h
@@ -3,14 +3,14 @@
  
 typedef struct {
     int  numero;
-    char bonus[5];
+    char bonus[5];  // 5 correponds au nombre de caractère du bonus
 } cartes;
  
-void creerPioche(cartes *tab);
-int  melanger(cartes *tab, int taille);
-int  effetNumero(int numero, int score);
-int  effetBonus(char *bonus, int score);
-int  carteExisteManche(cartes *main, int debut, int taille, cartes carte);
+void creerPioche(cartes *tab);   // remplit le tableau de 85 cartes qui constitue la pioche
+int  melanger(cartes *tab, int taille);  // mélange aléatoirement les cartes de la pioche
+int  effetNumero(int numero, int score);  // ajoute la valeur du numéro au score 
+int  effetBonus(char *bonus, int score);  // ajoute la valeur du bonus au score
+int  carteExisteManche(cartes *main, int debut, int taille, cartes carte);  // cherche si un joueur possède un doublon
  
 #endif
  
