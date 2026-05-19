1f19677693f8c03542c8c2ef791bd0b8551784db
raphael573 <raphael.yoyotte@etu.cyu.fr>
2026-05-19 15:26:58 +0200

    Update affichage.h

--- include/affichage.h
+++ include/affichage.h
@@ -1,10 +1,10 @@
 #ifndef AFFICHAGE_H
 #define AFFICHAGE_H
-
+ 
 #include "carte.h"
 #include "joueur.h"
-
-// Couleurs ANSI
+ 
+/* Couleurs ANSI */
 #define ROUGE_GRAS   "\x1b[1;31m"
 #define VERT_GRAS    "\x1b[1;32m"
 #define JAUNE_GRAS   "\x1b[1;93m"
@@ -16,17 +16,18 @@
 #define BG_GOLD      "\033[43m"
 #define TEXT_BLACK   "\033[30m"
 #define MAGENTA_GRAS "\x1b[38;5;210m"
-
-
+#define FOND_NOIR    "\x1b[40m"
+ 
 void afficher_une_carte(cartes c, int ligne);
 void afficher_ligne_carte(cartes *tab_cartes, int taille);
-void afficher_accueil();
+void afficher_accueil(void);
 void afficher_tour(joueur *j, int tour);
 void afficherMain(joueur *j);
 void afficherStats(cartes *tab, int n);
-void afficher_cartepiochée(cartes c);
+void afficher_cartepiocher(cartes c);
 void afficherRisque(joueur *j, cartes *tab, int n, int taille);
-void afficherScoresManche(joueur *joueurs, int nb_joueur, int indexLeader, int TAILLE);
-void afficherFinPartie(joueur *joueurs, int nb_joueur, int TAILLE);
-
+void afficherScoresManche(joueur *joueurs, int nb_joueur, int indexLeader, int taille);
+void afficherFinPartie(joueur *joueurs, int nb_joueur, int taille);
+ 
 #endif
+ 
