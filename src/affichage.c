af6609a57d41ba4ee1071de4b02d5decbd60a4ec
raphael573 <raphael.yoyotte@etu.cyu.fr>
2026-05-19 18:13:53 +0200

    Refactor affichage.c for improved readability

--- src/affichage.c
+++ src/affichage.c
@@ -3,7 +3,7 @@
 #include "joueur.h"
 #include "affichage.h"
  
-void afficher_une_carte(cartes c, int ligne) {
+void afficher_une_carte(cartes c, int ligne) {     // créer le design d'une carte sur une même ligne
     if (ligne == 0) {
         printf(" " OR "┌─────┐" RESET);
     } else if (ligne == 1 || ligne == 3) {
@@ -13,7 +13,7 @@ void afficher_une_carte(cartes c, int ligne) {
             printf(" " OR "│" BG_WHITE "     " RESET OR "│" RESET);
     } else if (ligne == 2) {
         if (c.bonus[0] != '\0')
-            printf(" " OR "│" BG_GOLD TEXT_BLACK " %-3s " RESET OR "│" RESET, c.bonus);
+            printf(" " OR "│" BG_GOLD TEXT_BLACK " %-3s " RESET OR "│" RESET, c.bonus);    // -3s sert à bloquer l'affichage pour éviter les décalages
         else
             printf(" " OR "│" BG_WHITE TEXT_BLACK " %2d  " RESET OR "│" RESET, c.numero);
     } else if (ligne == 4) {
@@ -21,7 +21,7 @@ void afficher_une_carte(cartes c, int ligne) {
     }
 }
  
-void afficher_ligne_carte(cartes *tab_cartes, int taille) {
+void afficher_ligne_carte(cartes *tab_cartes, int taille) {          // affiche la carte en colonne selon le nombre de lignes 
     for (int ligne = 0; ligne < 5; ligne++) {
         for (int m = 0; m < taille; m++)
             afficher_une_carte(tab_cartes[m], ligne);
@@ -29,7 +29,7 @@ void afficher_ligne_carte(cartes *tab_cartes, int taille) {
     }
 }
  
-void afficher_accueil(void) {
+void afficher_accueil(void) {     // void en paramètre = sécurité contre les bugs 
     printf("\n");
     printf(ROUGE_GRAS " ╔════════════════════════════════════════╗\n" RESET);
     printf(ROUGE_GRAS " ║                                        ║\n" RESET);
@@ -49,14 +49,13 @@ void afficher_tour(joueur *j, int tour) {
 }
  
 void afficherMain(joueur *j) {
-    printf(MAGENTA_GRAS " Vos cartes en main : (%d/7 numeros)\n" RESET,
-           compterNumeros(j));
+    printf(MAGENTA_GRAS " Vos cartes en main : (%d/7 numeros)\n" RESET, compterNumeros(j));
     int debutAff = j->nb_cartes - j->nbCartesManche;
     afficher_ligne_carte(&j->cartes[debutAff], j->nbCartesManche);
 }
  
 void afficherStats(cartes *tab, int n) {
-    int compteur[13] = {0};
+    int compteur[13] = {0};  //initialise un compteur à 0 pour chaques cartes numéros
  
     for (int i = 0; i < n; i++)
         if (tab[i].numero >= 0 && tab[i].numero <= 12)
@@ -65,7 +64,7 @@ void afficherStats(cartes *tab, int n) {
     printf("\n");
     printf(MAGENTA_GRAS " Statistiques de la pioche — cartes déjà sorties\n" RESET);
  
-    /* Première rangée : cartes 0..6 */
+    /* Première rangée : cartes 0 à 6 */
     for (int ligne = 0; ligne < 5; ligne++) {
         for (int i = 0; i <= 6; i++) {
             cartes temp;
@@ -84,7 +83,7 @@ void afficherStats(cartes *tab, int n) {
     }
     printf("\n");
  
-    /* Deuxième rangée : cartes 7..12 */
+    /* Deuxième rangée : cartes 7 à 12 */
     for (int ligne = 0; ligne < 5; ligne++) {
         for (int i = 7; i <= 12; i++) {
             cartes temp;
